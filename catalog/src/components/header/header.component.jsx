abf7270e9d6301457ce81a4dd0c428412a800056
Oscfran <Oscfran0209@gmail.com>
2022-06-09 10:56:34 -0600

    Catalog with a standard test data (not final)
    shows a catalog for the whisky web store with a sign in, and a cart shop. Database connectios and change test data required

---
+++ catalog/src/components/header/header.component.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { auth } from "../../firebase/firebase.utils.js";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component.jsx";
+import { selectCartHidden } from "../../redux/cart/cart.selectors.js";
+import { selectCurrentUser } from "../../redux/user/user.selector.js";
+
+import "./header.styles.scss";
+
+const Header = ({ currentUser, hidden }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+
+    <div className="options">
+
+      <Link className="option" to="/">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
+    </div>
+    {hidden ? null : <CartDropdown />}
+    <p className="king"></p> {/* Puts a text on the top right a refering to the shopping cart */}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
+});
+
+export default connect(mapStateToProps)(Header);
---
+++ catalog/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+
+const HomePage = ({ collections }) => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+)
+
+export default HomePage;
